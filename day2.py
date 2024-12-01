e17db25c17c958235e5fdf39a368054521c387a5
jgrue <155145828+jgrue@users.noreply.github.com>
2024-12-01 09:02:31 +0100

    day2 preparation

---
+++ day2.py
@@ -0,0 +1,20 @@
+
+# python training 
+# code of advent 2024 - day 1
+
+# Importing the re module
+import re
+
+#file = open("input_day2.txt", "r")
+file = open("temp.txt", "r")
+lines = file.readlines()
+
+print(" \n")
+
+sum = 0
+
+for line in lines:      
+    
+    print(line)
+
+print(f"Sum={sum}")
